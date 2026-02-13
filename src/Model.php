f82ca5555e7e068f9899934522b6da926717fbb8
Fize <chenfengzhan@qq.com>
2026-02-13 18:00:05 +0800

    feat: 模型规划。

--- src/Model.php
+++ src/Model.php
@@ -8,4 +8,32 @@ namespace Fize\Database;
 class Model
 {
 
+    /**
+     * 一对一
+     */
+    public function hasOne($class)
+    {
+
+    }
+
+    /**
+     * 一对多
+     */
+    public function hasMany($class)
+    {
+
+    }
+
+    /**
+     * 反向关联
+     */
+    public function belongsTo($class)
+    {
+
+    }
+
+    public function belongsToMany()
+    {
+
+    }
 }
